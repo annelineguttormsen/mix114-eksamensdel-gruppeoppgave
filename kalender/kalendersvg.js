357cc8466eff043f490042af8b4d3fe11c1ec952
Anneline <anneline.guttormsen@hotmail.no>
2021-04-20 14:34:03 +0200

    Oppsett kalender
    Basic oppsett, midlertidige farger

--- kalender/kalendersvg.js
+++ kalender/kalendersvg.js
@@ -13,16 +13,34 @@ function createSVGElement(tag) {
 
 function lagKalender() {
     console.log("lagkalender");
-    kalenderElement.appendChild(lagKalenderDag());
+    //fordi det er 30 dager uansett måned, gå gjennom 30 ganger
+    //oppdater y hver gang, reset på 7
+    //oppdater x når 
+    let x = 10;
+    let y = 10;
+    for (let i = 0;i <= 30;i++) {
+        console.log("forløkke er på", i);
+        kalenderElement.appendChild(lagKalenderDag(x, y));
+        if (i == 0) {
+            continue;
+            //% vil aktiveres på 0
+        }
+        if (i % 7 == 0) {
+            x = 10;
+            y += 160;
+        } else {
+            x += 160;
+        }
+    }
 }
 
-function lagKalenderDag() {
-    console.log("lagkalenderdag");
+function lagKalenderDag(x, y) {
     let rect = createSVGElement("rect");
     rect.setAttribute("height", "150");
     rect.setAttribute("width", "150");
     rect.setAttribute("fill", "lightblue");
-    rect.setAttribute("y", 10);
-    rect.setAttribute("x", 10);
+    console.log("lagkalenderdag, x og y er ", x, y);
+    rect.setAttribute("x", x);
+    rect.setAttribute("y", y);
     return rect
 }
