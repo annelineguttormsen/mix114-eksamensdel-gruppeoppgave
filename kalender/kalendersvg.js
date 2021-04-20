b84e37b2c731fe031e2121472575737d42392a0e
Anneline <anneline.guttormsen@hotmail.no>
2021-04-20 13:50:22 +0200

    Basic lagkalender funksjon

--- kalender/kalendersvg.js
+++ kalender/kalendersvg.js
@@ -1,3 +1,28 @@
+let kalenderData = new Array();
+const kalenderElement = document.getElementById("kalender__svg");
+
+/*sted 9999, år 671, 4 (april), i JSON*/
 fetch("https://trialmix.infomedia.uib.no/andregrader/9999/671/4/json")
 .then(response => response.json())
-.then(data => console.log(data));
+.then((data) => {kalenderData = data; lagKalender()});
+
+function createSVGElement(tag) {
+    let ns = "http://www.w3.org/2000/svg";
+    return document.createElementNS(ns,tag);
+}
+
+function lagKalender() {
+    console.log("lagkalender");
+    kalenderElement.appendChild(lagKalenderDag());
+}
+
+function lagKalenderDag() {
+    console.log("lagkalenderdag");
+    let rect = createSVGElement("rect");
+    rect.setAttribute("height", "150");
+    rect.setAttribute("width", "150");
+    rect.setAttribute("fill", "lightblue");
+    rect.setAttribute("y", 10);
+    rect.setAttribute("x", 10);
+    return rect
+}
