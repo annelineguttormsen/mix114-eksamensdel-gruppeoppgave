c22faceb6a40253ebb9c17bc2b11d62d6e17a39a
Anneline <anneline.guttormsen@hotmail.no>
2021-05-08 02:25:05 +0200

    oppdatering article percent line
    sjekk om bruker er ferdig å scrolle, regn deretter ut hvor lang article line på toppen skal være. transition satt i css

--- fortelling/waypoint demo/index.js
+++ fortelling/waypoint demo/index.js
@@ -3,17 +3,20 @@ $(function() {
 let articleScrollPercentageElement = $("#scroll__percent");
 let fortellingTekstScroll = $(".fortelling__tekst__scroll");
 
-articleScrollPercentageElement.css("width", "10%");
+let isScrolling;
 
 fortellingTekstScroll.on("scroll", fixArticlePercentage);
 
 function fixArticlePercentage() {
-    console.log(fortellingTekstScroll[0].scrollTop);
-    console.log(fortellingTekstScroll[0].scrollHeight);
-    let heightWithoutViewport = fortellingTekstScroll[0].scrollHeight - fortellingTekstScroll[0].clientHeight;
-    let percent = (fortellingTekstScroll[0].scrollTop / heightWithoutViewport)*100;
-    console.log("Dette er prosenten, ", percent);
-    articleScrollPercentageElement.css("width", (percent + "%"))
+    window.clearTimeout(isScrolling);
+
+    //https://gomakethings.com/detecting-when-a-visitor-has-stopped-scrolling-with-vanilla-javascript/
+	isScrolling = setTimeout(function() {
+        let heightWithoutViewport = fortellingTekstScroll[0].scrollHeight - fortellingTekstScroll[0].clientHeight;
+        let percent = (fortellingTekstScroll[0].scrollTop / heightWithoutViewport)*100;
+        console.log("Dette er prosenten, ", percent);
+        articleScrollPercentageElement.css("width", (percent + "%"))
+    }, 30);
 }
 
 });
